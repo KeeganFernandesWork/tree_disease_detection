a3b79b56e2cc3616e51ac9a5033f44ad97f4ed0a
KeeganFernandesWork <keeganfdes03@gmail.com>
2023-05-15 22:43:14 +0530

    final push

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template,request
 import os
 from model import model_app
 app = Flask(__name__)
-
+app.config['UPLOAD_FOLDER'] = 'uploads'
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
@@ -10,7 +10,8 @@ def home():
             file = request.files['file']
             filename = file.filename
             filepath = os.path.join('static/uploads', filename)  # Path to save the uploaded file
-            #Perform any additional processing on the uploaded file
+            file.save(filepath)
+            # Perform any addi<LeftMouse>tional processing on the uploaded file
             label = model_app(filepath)
             return render_template('home.html', filename=filename, output = label)
     return render_template('home.html')
